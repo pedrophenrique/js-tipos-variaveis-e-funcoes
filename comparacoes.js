1cd796a3dfa0661ffe1b0f3d405e366ed2a6bb58
pedrophenrique <pedrohenriquegomes-2000@hotmail.com>
2023-03-09 22:31:46 -0300

    Add mais informações sobre comparações

--- comparacoes.js
+++ comparacoes.js
@@ -14,5 +14,16 @@ console.log(typeof texto)
 
 // conversão explícita 
 
-Number()
-String()
+//Number()
+//String()
+
+// Até agora praticamos com alguns dos operadores do JavaScript, especialmente alguns dos utilizados em operações matemáticas:
+// Operador de adição	x + y
+// Operador de subtração	x - y
+// Operador de multiplicação	x * y
+// Operador de divisão	x / y
+
+// Porém, a lista de operadores é extensa e há muitas formas de utilizá-los. Além das operações matemáticas básicas, do = para atribuição de valor em uma variável e das comparações com == e ===, utilizamos operadores para diversas outras tarefas de código, por exemplo:
+// ||: Operador “ou”, retorna true caso uma condição seja válida;
+// &&: Operador “e”, retorna true somente se todas as condições forem válidas;
+// != e !==: Operadores “não igual” e “estritamente não igual”, utilizados para comparação, da mesma forma que == e === retornam true ou false.
